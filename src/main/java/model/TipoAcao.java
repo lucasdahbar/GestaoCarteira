6d780ee26cd62444a8ad9555eb9a5f73140050ba
Lucas Dahbar <lucasgdahbar@gmail.com>
2026-01-14 14:15:47 -0300

    Cria e implementa classe de constantes TipoAcao

---
+++ src/main/java/model/TipoAcao.java
@@ -0,0 +1,10 @@
+package model;
+
+public class TipoAcao {
+    public static final String ORDINARIA = "ORDINARIA";
+    public static final String PREFERENCIAL = "PREFERENCIAL";
+    public static final String UNIT = "UNIT";
+
+    private TipoAcao() {
+    }
+}
