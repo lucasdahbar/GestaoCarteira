a8226d2f17cd6aecef7aa3891bffc2c4a71b6049
Lucas Dahbar <lucasgdahbar@gmail.com>
2026-01-14 14:17:48 -0300

    Cria construtor e getTipoAcao em Ativo

--- src/main/java/model/Acao.java
+++ src/main/java/model/Acao.java
@@ -1,5 +1,16 @@
 package model;
 
-public class Acao extends Ativo{
+import java.math.BigDecimal;
 
+public class Acao extends Ativo{
+    private String tipoAcao;
+
+    public Acao(String nome, String ticker, BigDecimal precoAtual, boolean restritoQualificados) {
+        super(nome, ticker, precoAtual, restritoQualificados, TipoRenda.VARIAVEL, Origem.NACIONAL);
+        this.tipoAcao = definirTipoPeloTicker(ticker);
+    }
+
+    public String getTipoAcao() {
+        return tipoAcao;
+    }
 }
